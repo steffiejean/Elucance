ae981bcb12d68885cc45124d7182e62857936e39
steffiejean <steffie.jean@gmail.com>
2017-05-04 22:00:27 +1200

    server

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-// var $ = require('jquery')
+// // var $ = require('jquery')
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static(__dirname))
+
+
+
 var express = require('express');
 var app = express();
 
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
+
 app.use(express.static(__dirname))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + 'index.html');
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
