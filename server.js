2082d68e4c3bf542af69448bfffe460be91587cb
steffiejean <steffie.jean@gmail.com>
2017-05-04 21:11:19 +1200

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// var $ = require('jquery')
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
