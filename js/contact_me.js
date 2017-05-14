21bb58c1b6557bfea8056e9fdaac429db1108cef
steffiejean <steffie.jean@gmail.com>
2017-05-14 15:57:14 +1200

    update to success message

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you! Your message has been sent and we will be in touch within 48 hours.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
